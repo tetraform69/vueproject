607bec68f1799f37546aaf2eaa4ead4753cc6b0d
tetraform69 <oscorredor69@gmail.com>
2023-08-22 19:27:36 -0500

    ruta de logout y navguard implementado para no pasar del login sin seccion

--- src/components/NavUser.vue
+++ src/components/NavUser.vue
@@ -25,7 +25,7 @@ export default {
             },
             {
                 text: 'logout',
-                url: '/'
+                url: '/logout'
             },
         ]
     }),
--- src/router/index.js
+++ src/router/index.js
@@ -5,31 +5,54 @@ import LoginView from '../views/LoginView.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'login',
-    component: LoginView
-  },
-  {
-    path: '/user',
-    name: 'user',
-    // route level code-splitting
-    // this generates a separate chunk (User.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "User" */ '../views/UserView.vue')
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    // route level code-splitting
-    // this generates a separate chunk (Admin.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "Admin" */ '../views/AdminView.vue')
-  }
+    {
+        path: '/',
+        name: 'login',
+        component: LoginView
+    },
+    {
+        path: '/user',
+        name: 'user',
+        // route level code-splitting
+        // this generates a separate chunk (User.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "User" */ '../views/UserView.vue')
+    },
+    {
+        path: '/admin',
+        name: 'admin',
+        // route level code-splitting
+        // this generates a separate chunk (Admin.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "Admin" */ '../views/AdminView.vue')
+    },
+    {
+        path: '/logout',
+        name: 'logout',
+        component: {
+            beforeRouteEnter(to, from, next) {
+                localStorage.clear()
+
+                const destino = {
+                    path: '/',
+                }
+
+                next(destino)
+            }
+        }
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'login' && !localStorage.token){
+        next({name: 'login'})
+    }else{
+        next()
+    }
 })
 
 export default router
